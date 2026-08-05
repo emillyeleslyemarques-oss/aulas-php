fac9e62c1808b2bbe46579dc742c610b992169d9
emillyeleslyemarques-oss <emillyeleslyemarques@gmail.com>
2026-08-05 09:17:28 -0300

    Intercalando html e php

--- 08-intercalando-php-html.php
+++ 08-intercalando-php-html.php
@@ -27,7 +27,7 @@ echo "<p>Ele estuda na escola " . ESCOLA . "</p>";
     <hr>
 
     <h2>Usando PHP intercalando comandos com HTML</h2>
-<?php $idade = 25; ?>
+<?php $idade = 25; ?>  
 
     <h3>Resultado (usando PHP pra tudo):</h3>
 <?php
@@ -38,8 +38,31 @@ echo "<p>Ele estuda na escola " . ESCOLA . "</p>";
 }
     ?>
     <h3>Resultado (usando PHP só onde é necessário):</h3>
+<?php 
+if ($idade >=18):
+?>    
+    <p><b><?= $aluno ?></b> é maior de idade</p>
+<?php 
+else :
+?>    
+    <p><i><?= $aluno ?></i> é menor de idade</p>
+<?php 
+endif;
+?> 
 
-
+     <h3>Resultado (refatorado)</h3>
+     <i>(veja que, mudando a lógica e usando variáveis auxiliares, nem precisamos 
+        fazer as separações de PHP e HTML)</i>
+<?php 
+if ($idade >=18){
+     $htmlSaida = "<b><?= $aluno ?></b>";
+     $palavra = "maior";
+} else {
+     $htmlSaida = "<i>$aluno</i>";
+     $palavra = "menor";
+}
+?>  
+    <p> <?= $htmlSaida ?> é <?= $palavra ?> de idade</p>
 
 </body>
 
