7f3f0dacf141b78e7b32f30e484e5fdbe2f0db86
emillyeleslyemarques-oss <emillyeleslyemarques@gmail.com>
2026-08-05 11:35:49 -0300

    PHP com loops

---
+++ 09-loops.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loops no PHP</title>
+</head>
+<body>
+    <h1>Trabalhando com comandos de repetição</h1>
+    <hr>
+
+    <h2>While (enquanto)</h2>
+    <p>Executa ações repetidas vezes enquando a condição for verdadeira</p>
+<?php 
+$i = 1;
+while ($i <= 5):  //{ 
+?>
+    <p>Parágrafo: <?= $i ?></p>
+<?php 
+    $i++;
+endwhile;  //}
+?>
+</body>
+</html>
