906b4c797d2928b813f734dea0285b5b88470ba9
emillyeleslyemarques-oss <emillyeleslyemarques@gmail.com>
2026-08-05 11:54:39 -0300

    Loops PHP

--- 09-loops.php
+++ 09-loops.php
@@ -20,5 +20,41 @@ while ($i <= 5):  //{
     $i++;
 endwhile;  //}
 ?>
+
+
+    <hr>
+
+    <h2>do/while (faça/enquanto)</h2>
+    <p>Executa as ações pelo menos <b>uma vez</b> e, se
+     a condição for verdadeira, continua executando
+    outras vezes.</p>
+
+<?php 
+$j = 1;
+do {
+?>  
+    <div>
+        <h3>Qualquer</h3>
+        <p>Bloco <?= $j ?></p>
+    </div>
+<?php 
+  $j++;
+}while ($j <= 3)  
+?>
+
+    <hr>
+
+    <h2>for (para)</h2>
+    <p>Executa ações de acordo com uma quantidade
+    determinada de vezes.</p>
+<section>
+    <h3>FAQ (perguntas frequentes)</h3>
+<?php for($i = 1; $i <= 5; $i++){  //ou :?>
+     <details>
+        <summary>Pergunta <?= $i ?></summary>
+        <p>resposta <?= $i ?></p>
+     </details>
+<?php } //ou endfor;?>
+</section>
 </body>
 </html>
